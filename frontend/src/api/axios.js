3e860479c0f018071f65f5ea868b12fbcf2ca7fa
kyunhong <mynameishongkyunpark@gmail.com>
2026-04-13 19:48:57 +0900

    환경변수

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_API_URL || 'https://test-production-7665.up.railway.app',
 });
-
 export default api;
