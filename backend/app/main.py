7155e4ef01afe8e7960a805372725f838e9aa82d
kyunhong <mynameishongkyunpark@gmail.com>
2026-04-13 15:47:21 +0900

    add

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
+        "https://kyunhong.github.io",
         os.environ.get("FRONTEND_URL", "*"),
     ],
     allow_credentials=True,
